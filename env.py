ac37216804f21757b876a59548826837e2dd1ccf
weier123 <55678900+weier123@users.noreply.github.com>
2022-03-01 22:02:53 +0800

    Create env.py

---
+++ env.py
@@ -0,0 +1,11 @@
+#env.py
+import pyglet
+class Viewer(pyglet.window.Window):
+  def __init__(self,arm_info):
+    #画出手臂等
+  def render(self):
+    #刷新并呈现在屏幕上
+  def on_draw(self):
+    #刷新手臂等位置
+  def _update_arm(self):
+    #更新手臂的位置信息
